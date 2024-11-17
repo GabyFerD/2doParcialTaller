a14d19c71057eeae5f58fb57e5f2bafda697e0fe
GabyFerD <134448230+GabyFerD@users.noreply.github.com>
2024-11-17 20:28:23 -0300

    2do parcial Taller de lenguaje y marcado

---
+++ 33797896/js/scripts.js
@@ -0,0 +1,18 @@
+//INDEX
+
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "mel" && password === "123") {
+      window.location.href = `admin.html?user=${username}`;
+    } else {
+      alert("Usuario incorrecto");
+    }
+  });
+
+//ADMIN
+document.getElementById("welcomeMessage").innerText = `Bienvenido/a ${user}!`;
---
+++ 33797896/js/scripts1.js
@@ -0,0 +1,4 @@
+//LIBROJUEGO CAP.1
+// audio - para reproducir en bucle
+const audio2 = document.getElementById("suspenseAudio2");
+audio2.loop = true;
